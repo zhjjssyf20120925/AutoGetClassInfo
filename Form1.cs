068ae8432c6d1bb24cd71836fadb4a4befe2281f
zhjjssyf20120925 <875135458@qq.com>
2015-08-03 10:11:53 +0800

    新增Student类。
    新增Student类。

--- Form1.cs
+++ Form1.cs
@@ -11,9 +11,28 @@ namespace AutoGetClassInfo
 {
     public partial class Form1 : Form
     {
+        #region [  成员变量  ]
+
+        #endregion
+
+        #region [  私有方法  ]
+        private void GetClassInfo()
+        {
+
+
+        }
+        #endregion
+
+        #region [  成员方法  ]
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
     }
 }
---
+++ Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoGetClassInfo
+{
+    /// <summary>
+    /// 学生类
+    /// </summary>
+    public class Student
+    {
+        private int student_ID;
+
+        /// <summary>
+        /// 获取或者设置一个值 学生编号
+        /// </summary>
+        public int Student_ID
+        {
+            get { return student_ID; }
+            set { student_ID = value; }
+        }
+
+        private string student_Name;
+
+        /// <summary>
+        /// 获取或者设置一个值  学生姓名
+        /// </summary>
+        public string Student_Name
+        {
+            get { return student_Name; }
+            set { student_Name = value; }
+        }
+
+        private string student_Sex;
+
+        /// <summary>
+        /// 获取或者设置一个值  学生性别
+        /// </summary>
+        public string Student_Sex
+        {
+            get { return student_Sex; }
+            set { student_Sex = value; }
+        }
+    }
+}
